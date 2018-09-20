c429dd6a57b79f5e13ad7c357af7b47bb7fc79b8
JanHoskovec <jean.hoskovec@gmail.com>
2018-09-20 10:18:11 +0200

    Challenge MVC.Net

---
+++ JeuDroides/ChallengeMvcNet/Controllers/DroidController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using JeuDroides.Core.Models;
+
+namespace ChallengeMvcNet.Controllers
+{
+    public class DroidController : Controller
+    {
+        // GET: Droid
+        public ActionResult Index()
+        {
+            List<Droide> myDroides = Droide.GetAllM();
+            ViewBag.Droids = myDroides;
+            return View();
+        }
+    }
+}
---
+++ JeuDroides/JeuDroides.Core/Models/Droide.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace JeuDroides.Core.Models
+{
+    public class Droide : Espece
+    {
+        public string Matricule { get; set; }
+        public DateTime DateCreation { get; set; }
+        public DateTime DerniereMiseAJour { get; set; }
+        // TODO modeliser l'équipe de combat
+
+        public static List<Droide> GetAllM ()
+        {
+            List<Droide> MyList = new List<Droide>();
+
+            using (SqlConnection connection = new SqlConnection(Properties.Settings.Default.MaChaineParDefautDeConnexion))
+            {
+                connection.Open();
+                using (SqlCommand command = connection.CreateCommand())
+                {
+                    command.CommandText = "select Nom from Droide where Nom like 'm%'";
+
+                    using (SqlDataReader reader = command.ExecuteReader())
+                    {
+                        while(reader.Read())
+                        {
+                            Droide DroideEnCours = new Droide();
+                            DroideEnCours.Nom = reader["Nom"].ToString();
+                            MyList.Add(DroideEnCours);
+                        }
+                    }
+
+                }
+            }
+
+            return MyList;
+        }
+    }
+}
